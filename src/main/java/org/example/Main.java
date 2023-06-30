d6aa1071054848640318f55663c6d4d0f93b57ae
Goguzgungor <goguzgungor59@gmail.com>
2023-06-30 15:18:01 +0300

    ThreeSum.java ve SumOfTwo.java soruları çözüldü

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,15 @@
 package org.example;
+import java.util.Collections;
 
-import questions.LongestCommonPrefix;
-import questions.RomanToInteger;
+import questions.ThreeSum;
+import questions.sumoftwo.ListNode;
+import questions.sumoftwo.SumOfTwo;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
-        String text = longestCommonPrefix.longestCommonPrefix(new String[]{"flower","flow","flight"});
-        System.out.println(text);
+        ThreeSum threeSum = new ThreeSum();
+        System.out.println(threeSum.threeSum(new int[]{1,-1,-1,0}));
     }
 }
